da058b8a3c14d2875dcf30c9d5aaae5226f6f3d2
Jessica Perez <1jessicaperez@gmail.com>
2020-07-10 21:54:01 -0400

    README updated

--- README.md
+++ README.md
@@ -1,46 +1,27 @@
-# 05 Third-Party APIs: Work Day Scheduler
+## Build a web Daily planner
 
 Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
-You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+### What I did
 
-## User Story
+``
+Once the app opened in the browser, it features dynamically updated HTML and CSS (using Jquery).
+I used Bootstrap components to build this responsive layout.
+Local storage is used to stored the data, even after refreshing the page.
 
-```
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
+I attempted to create the divs through JS, the code was partially working, only needed to figure out how to populate with correct time of the working hours.
 
-## Acceptance Criteria
+``
 
-```
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-```
+### Project screenshots
 
-The following animation demonstrates the application functionality:
+![Day Planner screenshot](planner.png)
+![Planner local storage screenshot](planner-storage.png)
 
-![day planner demo](./Assets/05-third-party-apis-homework-demo.gif)
+## Project submissions
 
-## Review
+- The URLs of the deployed applications:
+  https://jessicaperez1.github.io/Your-Day-Planner/
 
-You are required to submit the following for review:
-
-* The URL of the deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a README describing the project.
-
-- - -
-© 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
+- The URLs of the GitHub repositories:
+  https://github.com/JessicaPerez1/Your-Day-Planner.git
--- planner.js
+++ planner.js
@@ -1,76 +1,37 @@
+//get JS running after the document is ready to execute JS code
 $(document).ready(() => {
   console.log("ready!");
 });
 
 //DOM elements
-var container = document.querySelector(".container");
-var inputGroup = document.querySelector(".time-block");
+var container = $(".container");
+var inputGroup = $(".time-block");
 var currentDateEl = $("#currentDay");
-var plannerEl = document.querySelector("#planner");
-//set my variables
-// var workingHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
-//array of times
-// var timeFull = [
-//   00,
-//   01,
-//   02,
-//   03,
-//   04,
-//   05,
-//   06,
-//   07,
-//   08,
-//   09,
-//   10,
-//   11,
-//   12,
-//   13,
-//   14,
-//   15,
-//   16,
-//   17,
-//   18,
-//   19,
-//   20,
-//   21,
-//   22,
-//   23,
-//   24,
-// ];
+var currentTimeEl = $("#currentTime");
+var plannerEl = $("#planner");
 
-//NON WORKING CODE
-// var timeBlockEl = $(
-//   '<div class="input-group mb-2"><div class="input-group-prepend"><span class="input-group-text"></span></div> <textarea class="form-control" aria-label="With textarea"></textarea><input class="btn btn-primary" type="submit" value="Submit"></div>'
-// );
+//NON WORKING CODE - trying to generate the divs through JS
+// var workingHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+//create a div element with timeblock, input area and submit button
 // var timeBlockEl = $(
 //   '<div class="input-group mb-2"><div class="input-group-prepend"><span class="input-group-text"></span></div> <textarea class="form-control" aria-label="With textarea"></textarea><input class="btn btn-primary" type="submit" value="Submit"></div>'
 // );
 //for each loop to create 9 timeBlocks
-//   for (var i = 0; i < workingHours.length; i++) {
-//     //creating my new div
-//     inputGroup.innerHTML = workingHours[i];
-//     container.append(timeBlockEl);
-//     console.log([i]);
-//   }
-// });
-//as soon as i open my page get document on ready, create and append my 9 divs
-// for (var i = 9; i < 18; i++) {
-//   if (i < 12) {
-//     $(".input-group-text").text(workingHours[i] + "AM");
-//   }
-//   if (i > 12) {
-//     $(".input-group-text").text(workingHours[i] + "PM");
-//   }
-//   $(".container").append(timeBlockEl);
+// for (var i = 0; i < workingHours.length; i++) {
+//   //creating my new div
+//   inputGroup.text(workingHours[i]);
+//   container.append(timeBlockEl);
+//   console.log([i]);
 // }
 
 //GET THE CURRENT DATE and display it
 var date = moment();
 currentDateEl.text(date.format("dddd MMMM Do YYYY"));
-//MELISSA CODE
-// var now = moment().format("H");
+var now = moment().format("H");
+console.log(now);
+currentTimeEl.text(now + ":00 (military time)");
 
-// //TO GET CURRENT TIME - DONE
+//GET THE CURRENT TIME and display it
 function myHour() {
   var d = new Date();
   var n = d.getHours();
@@ -78,25 +39,6 @@ function myHour() {
 }
 console.log(myHour());
 
-// function init() {
-//   $("#currentDay").text(date);
-//   $(".time-block").each(buildRow);
-//   $(".btn").click(save);
-// }
-
-//MELISSAS CODE
-// function buildRow() {
-//   var time = $(this).attr("data-hour");
-//   time = parseInt(time);
-//   if (now === time) {
-//     $(this).addClass("present");
-//   }
-//   if (now > time) {
-//     $(this).addClass("past");
-//   }
-//   if (now < time) {
-//     $(this).addClass("future");
-//   }
 //set color blocks by comparing current time with the time in blocks
 $(".time-block").each(function () {
   console.log(this);
@@ -124,7 +66,7 @@ $(".time-block").each(function () {
 });
 
 var userInputByHour = [];
-
+//LOCALSTORAGE
 // jQuery Save button savebutton is targeted, when save button is clicked
 $(".btn").on("click", function () {
   //get nearby values
@@ -138,7 +80,6 @@ $(".btn").on("click", function () {
 });
 
 // // calling value of hour-9 and placing it in the proper hour description value
-
 $("#hour-9 .form-control").val(localStorage.getItem("hour-9"));
 $("#hour-10 .form-control").val(localStorage.getItem("hour-10"));
 $("#hour-11 .form-control").val(localStorage.getItem("hour-11"));
@@ -148,26 +89,3 @@ $("#hour-14 .form-control").val(localStorage.getItem("hour-14"));
 $("#hour-15 .form-control").val(localStorage.getItem("hour-15"));
 $("#hour-16 .form-control").val(localStorage.getItem("hour-16"));
 $("#hour-17 .form-control").val(localStorage.getItem("hour-17"));
-
-//display hour on top of page
-//generate the div - create 9 sub divs
-//in each sub div display time on the left, input area in the middle, submit button the the right
-//depending on the time of the day, it will diplay differently
-
-// STYLING - for loop that iterates through the time array to generate the div colors
-// function colorBlock (){
-// for (var i = 0; i <time.length; i++){
-//     if (myHour === time[i]){
-// //if time.key === myHour, then block is red
-//     if (myHour < time[i]) {
-// //if time.key < myHour, then block is grey
-//     if (myHour > time[i]) {
-// // if time.key> if time.key < myHour, then block is green
-//     }
-//     if (myHour < 09 || myHour>17){
-// //turn  all the divs to yellow
-//     }
-// }
-//     }
-
-// //LOCALSTORAGE
